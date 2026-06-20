4ef359d64cbd16525547579ffeb218803d8f442e
Endert0m <dani.tommaso@dmike.it>
2026-06-20 12:59:17 +0200

    Ridimensionamento bacino

--- src/collections/methods/lower_body.cpp
+++ src/collections/methods/lower_body.cpp
@@ -3,15 +3,15 @@
 Lower_Body::Lower_Body(rb::Vector3 pos,std::vector<gamba_data> data){
     if (data.size() != 2) throw "Lower_Body_Error: data vector size must be 2";
 
-    sx = new Gamba({pos[0],pos[1],pos[2]+150},data[0].dataPos,data[0].cosciaData,data[0].cavigliaData);
-    dx = new Gamba({pos[0],pos[1]+200,pos[2]+150},data[1].dataPos,data[1].cosciaData,data[1].cavigliaData);
-    t = new Torso({pos[0],pos[1]+100,pos[2]},_Float16(3.0));
+    sx = new Gamba({pos[0],pos[1]-60,pos[2]+150},data[0].dataPos,data[0].cosciaData,data[0].cavigliaData);
+    dx = new Gamba({pos[0],pos[1]+60,pos[2]+150},data[1].dataPos,data[1].cosciaData,data[1].cavigliaData);
+    t = new Torso({pos[0],pos[1],pos[2]},_Float16(3.0));
 
     PieceInterface* psx = sx->getJointPiece();
     PieceInterface* pdx = dx->getJointPiece();
 
-    jsx = new PivotJoint(t, {psx}, rb::Vector3{0,-100,50});
-    jdx = new PivotJoint(t, {pdx}, rb::Vector3{0,100,50});
+    jsx = new PivotJoint(t, {psx}, rb::Vector3{0,-60,50});
+    jdx = new PivotJoint(t, {pdx}, rb::Vector3{0,60,50});
 }
 
 
--- src/pieces/headers/torso.hpp
+++ src/pieces/headers/torso.hpp
@@ -8,7 +8,7 @@
 
 class Torso : public PieceInterface{
     private:
-        const sf::Vector3f torso_Dim = {100, 100, 250};
+        const sf::Vector3f torso_Dim = {100, 100, 150};
         const sf::Color torso_Col = sf::Color::Red;
     public:
         Torso(rb::Vector3 coords, _Float16 mass);
